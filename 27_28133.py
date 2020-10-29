34c1fd6a15732dc9b65a8873a3ae10734d0d8245
StombieIT <65386839+StombieIT@users.noreply.github.com>
2020-10-29 20:05:59 +0300

    Update 27_28133.py

--- 27_28133.py
+++ 27_28133.py
@@ -13,4 +13,4 @@ for i in range(len(ostd120) // 2):
 	if ostd120[i] != 0 and ostd120[-(i + 1)] != 0 and ostd120[i] + ostd120[-(i + 1)] > n1 + n2:
 		n1, n2 = ostd120[i], ostd120[-(i + 1)]
 
-print(n1, n2)
+print(n1, n2) if n1 != 0 and n2 != 0 else print('00')
