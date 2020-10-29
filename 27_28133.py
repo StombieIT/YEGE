03580f514ff02d642b5bfea9a359ffa4af7639fc
StombieIT <65386839+StombieIT@users.noreply.github.com>
2020-10-29 17:28:15 +0300

    Update 27_28133.py

--- 27_28133.py
+++ 27_28133.py
@@ -7,11 +7,10 @@ with open('28133_A.txt', 'r') as file:
 		if num > ostd120[ost]:
 			ostd120[ost] = num
 
-n1 = n2 = mxs = 0
+n1 = n2 = 0
 
 for i in range(len(ostd120) // 2):
-	if ostd120[i] != 0 and ostd120[-(i + 1)] != 0 and ostd120[i] + ostd120[-(i + 1)] > mxs:
-		mxs = ostd120[i] + ostd120[-(i + 1)]
+	if ostd120[i] != 0 and ostd120[-(i + 1)] != 0 and ostd120[i] + ostd120[-(i + 1)] > n1 + n2:
 		n1, n2 = ostd120[i], ostd120[-(i + 1)]
 
 print(n1, n2)
