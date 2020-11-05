e4b41bef033b680216f594029e05815e513d3172
StombieIT <65386839+StombieIT@users.noreply.github.com>
2020-11-05 12:48:36 +0300

    Update 27_28129.py

--- 27_28129.py
+++ 27_28129.py
@@ -1,6 +1,5 @@
 b = 160
 p = 7
-
 with open('28129_B.txt', 'r') as file:
 	next(file)
 	ostsmx = [0 for _ in range(b)]
@@ -14,13 +13,10 @@ with open('28129_B.txt', 'r') as file:
 		else:
 			if num > ostsmx[ost]:
 				ostsmx[ost] = num
-
 n1 = n2 = 0
-
 for i in range(len(ostsmx)):
 	for j in range(len(ostsmxp)):
 		if i != j and ostsmx[i] != 0 and ostsmxp[j] != 0:
 			if ostsmx[i] + ostsmxp[j] > n1 + n2:
 				n1, n2 = ostsmx[i], ostsmxp[j]
-
 print(n1, n2)
